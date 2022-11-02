4de1d59a9290c0017c82a23acc8dd433035548f6
Shun Hoshina <s21c1105da@s.chibakoudai.jp>
2022-11-02 10:00:51 +0900

    create report file

---
+++ scanword.c
@@ -0,0 +1,16 @@
+/*======================================================================
+Project Name    : <Numerical Analysis class report2>
+File Name       : <scancode.c>
+Encoding        : <utf-8>
+Creation Date   : <2022/11/02>
+ 
+Copyright © <2022> <Shun Hoshina>. All rights reserved.
+ 
+This source code or any portion thereof must not be  
+reproduced or used in any manner whatsoever.
+======================================================================*/
+
+#include <stdio.h>
+int main (void){
+    
+}
