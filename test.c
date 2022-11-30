0e95000f9dd78a65de4c491986f371e3fa903828
Shun Hoshina <s21c1105da@s.chibakoudai.jp>
2022-11-30 10:23:52 +0900

    c言語で課題１作成

--- test.c
+++ test.c
@@ -17,32 +17,44 @@ reproduced or used in any manner whatsoever.
 int main(void) {
 	FILE *fp; // FILE型構造体
 	char fname[] = "test.txt";
-	int chr, i;
+	int  i;
+    char chr[100];
  
 	fp = fopen(fname, "r"); // ファイルを開く。失敗するとNULLを返す。
 	if(fp == NULL) {
 		printf("%s file not open!\n", fname);
 		return -1;
 	}
-	//for(i=0;i<10;i++){
+	
 		
-	if(isalpha(fgetc(fp))==0){
-		//fgetc(fp)=' ';
-	//}
-	}
-	for(i=0;i<10;i++){
-		printf("%c", fgetc(fp));
+	/*if(isalpha(fgetc(fp))==0){
+	
+	}*/
+    
+	for(i=0;i<10;i++){  //配列に置換
+        fscanf(fp, "%c", &chr[i]);
 	
 	}
-	chr = fgetc(fp);
     
-    ///printf("%c", fname[2]);
-    //printf("%c", fname[3]);
-    //printf("%c", fname[4]);
+    for(i=0;i<10;i++){  //print
+		printf("%c", chr[i]);
+	
+	}
+    printf("\n");
+	for(i=0;i<10;i++){  //判別
+        if(isalpha(chr[i])==0){
+            chr[i]=' ';
+	    }
+	
+	}
 
-    //while((chr = fgetc(fp)) != EOF) {
-	//	putchar(chr);
-	//}
+    for(i=0;i<10;i++){  //print
+		printf("%c", chr[i]);
+	
+	}
+    printf("\n");
+    
+    
 	fclose(fp); // ファイルを閉じる
  
 	return 0;
